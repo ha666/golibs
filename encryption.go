0498baffc3e8bb718e0b6716e1572814669019d3
ha666 <ha666@ha666.com>
2017-01-01 23:03:44 +0800

    增加base64、hmcasha1、hmacsha256方法。

--- encryption.go
+++ encryption.go
@@ -1,13 +1,34 @@
 package golibs
 
 import (
+	"crypto/hmac"
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
 )
 
-//md5方法
 func Md5(s string) string {
 	h := md5.New()
 	h.Write([]byte(s))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func Base64(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
+
+func HmacSha1(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha1.New, key)
+	h.Write([]byte(message))
+	return Base64(h.Sum(nil))
+}
+
+func HmacSha256(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	return Base64(h.Sum(nil))
 }
