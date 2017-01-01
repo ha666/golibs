5dec42efc9180fcf08a215754cb2c671099ab61f
ha666 <ha666@ha666.com>
2017-01-02 00:54:16 +0800

    增加hmacsha512方法

--- encryption.go
+++ encryption.go
@@ -5,6 +5,7 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base64"
 	"encoding/hex"
 )
@@ -19,16 +20,20 @@ func Base64(src []byte) string {
 	return base64.StdEncoding.EncodeToString(src)
 }
 
-func HmacSha1(message string, secret string) string {
-	key := []byte(secret)
-	h := hmac.New(sha1.New, key)
-	h.Write([]byte(message))
-	return Base64(h.Sum(nil))
+func HmacSha1(message, secret []byte) []byte {
+	h := hmac.New(sha1.New, secret)
+	h.Write(message)
+	return h.Sum(nil)
 }
 
-func HmacSha256(message string, secret string) string {
-	key := []byte(secret)
-	h := hmac.New(sha256.New, key)
-	h.Write([]byte(message))
-	return Base64(h.Sum(nil))
+func HmacSha256(message, secret []byte) []byte {
+	h := hmac.New(sha256.New, secret)
+	h.Write(message)
+	return h.Sum(nil)
+}
+
+func HmacSha512(message, secret []byte) []byte {
+	h := hmac.New(sha512.New, secret)
+	h.Write(message)
+	return h.Sum(nil)
 }
