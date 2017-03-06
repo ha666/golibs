6eee6eedd73b2df5ae6da10288a30106982ec59c
ha666 <ha666@ha666.com>
2017-03-06 15:15:26 +0800

    增加方法：返回当前时间字符串

--- time.go
+++ time.go
@@ -23,6 +23,11 @@ func Unix() int64 {
 	return time.Now().Unix()
 }
 
+// 返回当前时间字符串
+func StandardTime() string {
+	return time.Now().Format(Time_TIMEStandard)
+}
+
 // 返回从2000-01-01 00:00:00到现在经过的纳秒数
 func From2000Nano() int64 {
 	timeA, _ := time.Parse(Time_TIMEStandard, "2000-01-01 00:00:00")
