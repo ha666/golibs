5084bd905d1125a5a8f7f4001c4fca76dc244e72
张肖辉 <zhangxiaohui@shopex.cn>
2017-04-12 18:19:44 +0800

    增加方法，转换32位的md5(或guid)到11个汉字，可逆转

--- string.go
+++ string.go
@@ -1,8 +1,12 @@
 package golibs
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
 	"errors"
+	"fmt"
 	"io"
 	"math/big"
 	"regexp"
@@ -43,7 +47,7 @@ func Strim(str string) string {
 	return str
 }
 
-func Unicode(rs string) string {
+func String2Unicode(rs string) string {
 	json := ""
 	for _, r := range rs {
 		rint := int(r)
@@ -104,4 +108,111 @@ func IsTaobaoNick(taobaoNick string) bool {
 		return false
 	}
 	return regexp.MustCompile(`(^[\\u4e00-\\u9fa5\\w_—\\-，。…·〔〕（）！@￥%……&*？、；‘“]*$)`).MatchString(taobaoNick)
+}
+
+const zipOffset int = 19968
+
+func ZipMd5(md5String string) (zipString string, err error) {
+	md5Bytes := getHexBytes(md5String + "0")
+	var data bytes.Buffer
+	var total int = 0
+	for index := 0; index < len(md5Bytes); index++ {
+		switch index % 3 {
+		case 0:
+			intByte, err := strconv.Atoi(fmt.Sprintf("%d", md5Bytes[index]))
+			if err != nil {
+				return zipString, err
+			}
+			total = intByte
+			break
+		case 1:
+			intByte, err := strconv.Atoi(fmt.Sprintf("%d", md5Bytes[index]))
+			if err != nil {
+				return zipString, err
+			}
+			total += intByte << 4
+			break
+		case 2:
+			intByte, err := strconv.Atoi(fmt.Sprintf("%d", md5Bytes[index]))
+			if err != nil {
+				return zipString, err
+			}
+			total += (intByte << 8) + zipOffset
+			uniString := fmt.Sprintf("\\u%x", total)
+			chineseString, err := Unicode2String(uniString)
+			if err != nil {
+				return zipString, err
+			}
+			data.WriteString(chineseString)
+			total = 0
+			break
+		}
+	}
+	zipString = data.String()
+	return
+}
+
+func UnZipMd5(zipString string) (md5String string, err error) {
+	var data bytes.Buffer
+	unicodeString := String2Unicode(zipString)
+	unicodeStrings := strings.Split(unicodeString, "\\u")
+	for _, unicodeValue := range unicodeStrings {
+		if strings.EqualFold(unicodeValue, "") {
+			continue
+		}
+		dec, err := strconv.ParseInt(unicodeValue, 16, 32)
+		if err != nil {
+			return md5String, err
+		}
+		dec -= int64(zipOffset)
+		data.WriteString(tenValue2Char(dec & 15))
+		data.WriteString(tenValue2Char((dec >> 4) & 15))
+		data.WriteString(tenValue2Char((dec >> 8) & 15))
+	}
+	md5String = SubString(data.String(), 0, 32)
+	return
+}
+
+func Unicode2String(form string) (to string, err error) {
+	bs, err := hex.DecodeString(strings.Replace(form, `\u`, ``, -1))
+	if err != nil {
+		return
+	}
+	for i, bl, br, r := 0, len(bs), bytes.NewReader(bs), uint16(0); i < bl; i += 2 {
+		binary.Read(br, binary.BigEndian, &r)
+		to += string(r)
+	}
+	return
+}
+
+func getHexBytes(str string) []byte {
+	result := []byte(str)
+	for index := 0; index < len(result); index++ {
+		if result[index] < 58 {
+			result[index] -= 48
+		} else {
+			result[index] -= 55
+		}
+	}
+	return result
+}
+
+func tenValue2Char(ten int64) string {
+	switch ten {
+	case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9:
+		return strconv.FormatInt(ten, 10)
+	case 10:
+		return "A"
+	case 11:
+		return "B"
+	case 12:
+		return "C"
+	case 13:
+		return "D"
+	case 14:
+		return "E"
+	case 15:
+		return "F"
+	}
+	return ""
 }
