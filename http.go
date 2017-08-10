9a460b8d1762e4f2ad712d5e83daf9c3a9622bae
张肖辉 <zhangxiaohui@shopex.cn>
2017-08-10 11:39:58 +0800

    增加方法：BuildRequestUrl

--- http.go
+++ http.go
@@ -21,6 +21,31 @@ func Get(requestUrl string) (int, string, error) {
 	return response.StatusCode, string(body), nil
 }
 
+//获取url和参数列表对应的完整请求url
+func BuildRequestUrl(requestUrl string, params url.Values) string {
+	if len(params) <= 0 {
+		return requestUrl
+	}
+	data := NewStringBuilder()
+	data.Append(requestUrl)
+	if !strings.Contains(requestUrl, "?") {
+		data.Append("?")
+	}
+	has_param := false
+	for k, v := range params {
+		if len(k) > 0 && len(v[0]) > 0 {
+			if has_param {
+				data.Append("&")
+			}
+			data.Append(k)
+			data.Append("=")
+			data.Append(url.QueryEscape(v[0]))
+			has_param = true
+		}
+	}
+	return data.ToString()
+}
+
 //获取url对应的内容，返回信息：StatusCode，body，err
 func Post(requestUrl string, params url.Values) (int, string, error) {
 	client := &http.Client{}
