81e3bd95d926098869e6ddc0bba2a4c2adf30165
ha666 <ha666@ha666.com>
2018-10-21 08:10:27 +0800

    增加方法：获取程序运行路径

--- http.go
+++ http.go
@@ -163,3 +163,12 @@ func PostFile(requestUrl string, params url.Values, field_name, path string) (in
 func Method(r *http.Request) string {
 	return r.Method
 }
+
+// 获取程序运行路径
+func GetCurrentDirectory() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return ""
+	}
+	return strings.Replace(dir, "\\", "/", -1)
+}
